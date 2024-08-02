c218eb66b3c6219a3818cc14ac8381455b086ae2
suryaxsingh <suryasingh7022@gmail.com>
2024-08-02 22:27:55 +0530

    connected to mongodb server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const ServerConfig = require('./config/serverconfig');
+const connectDB = require('./config/dbConfig');
 
 const app = express();
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
+    await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}...!!`);
-})
+});
