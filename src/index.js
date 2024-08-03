dad43af64c492522d578b939c43cc53bd092b611
suryaxsingh <suryasingh7022@gmail.com>
2024-08-03 10:50:16 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const connectDB = require('./config/dbConfig');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/ping', (req, res) => {
+    console.log(req.body);
+    return res.json({message: "pong"});
+})
+
 app.listen(ServerConfig.PORT, async () => {
     await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}...!!`);
